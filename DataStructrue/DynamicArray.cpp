1a65f2216e1133b11e5485fa3470a4cb2571bc1d
Wendaolee <leewendao@outlook.com>
2022-03-28 21:02:20 +0800

    Update DynamicArray.cpp
    If no accident,that's the final version?

--- DataStructrue/DynamicArray.cpp
+++ DataStructrue/DynamicArray.cpp
@@ -44,13 +44,15 @@ public:
     bool delete_last();
 
     bool insert_at(int i, int value);
-    bool delete_at(int i, int value);
+    bool delete_at(int i);
 
+    void reAllocated();
+    void reAllocated(int space);
     void output();
 
 private:
-    int array_size; // array_size is to point out the what is the (be used)end---With minus 1.
-    int array_max;  // array_max is to point out the array's truly end(no matter if valued)
+    int array_size; // array_size is to point out the truly nums of defined-elements it used.
+    int array_max;  // array_max is to point out the array's containing space.
     int *array;
 };
 
@@ -58,6 +60,7 @@ DynamicArray::DynamicArray(int max)
 {
     array = new int[max];
     array_max = max;
+    array_size = 0;
 }
 
 DynamicArray::DynamicArray(const int x[], int size)
@@ -77,27 +80,36 @@ int DynamicArray::get_at(int i)
     return array[i];
 }
 
-bool DynamicArray::set_at(int i, int value)
+void DynamicArray::reAllocated(int space)
 {
-
-    if (i >= array_max)
+    if (array_max > space)
     {
-        array_max = array_max * 2;
-        int *temp = new int[array_max]; // temp contains new array
+        return;
+    }
+    else
+    {
+        int *temp = new int[space];
 
-        // Copy old values to new array.
+        array_max = space;
+
         for (int i = 0; i < array_size; i++)
         {
             temp[i] = array[i];
         }
+        delete[] array;
+        array = temp;
+        return;
+    }
+}
 
-        delete[] array; // delete old array
-
-        array = temp; // point to new array
+bool DynamicArray::set_at(int i, int value)
+{
+    if (i >= array_max)
+    {
+        reAllocated(i + 1);
 
         array[i] = value;
         array_size = i + 1;
-
         return true;
     }
     else if (i > array_size)
@@ -126,47 +138,81 @@ bool DynamicArray::insert_first(int value)
 
     delete[] array;
     array = temp;
+    array_size++;
 };
 
 bool DynamicArray::delete_first()
 {
-    int *temp = new int[array_max];
     for (int i = 0; i < array_size; i++)
     {
-        temp[i] = array[i + 1];
+        array[i] = array[i + 1];
     }
 
-    delete[] array;
-    array = temp;
+    array_size--;
 }
 
 bool DynamicArray::insert_last(int value)
 {
     if (array_size >= array_max)
     {
-        set_at(array_size - 1,value);
+        reAllocated(2 * array_max);
+        set_at(array_size - 1, value);
+        array_size++;
     }
-    else{
+    else
+    {
         array[array_size - 1] = value;
     }
-    
 }
 
 bool DynamicArray::delete_last()
 {
     array[array_size - 1] = NULL;
+    array_size--;
 }
 
-bool DynamicArray::insert_at(int i,int value)
+bool DynamicArray::insert_at(int i, int value)
 {
-    
+    if (i > array_max)
+    {
+        set_at(i, value);
+        return true;
+    }
+    else if ((array_size + 1) >= array_max)
+    {
+        reAllocated(2 * array_max);
+        for (int i = array_size; i > i; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[i] = value;
+        array_size++;
+    }
+    else
+    {
+        for (int i = array_size; i > i; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[i] = value;
+        array_size++;
+    }
+}
+
+bool DynamicArray::delete_at(int i)
+{
+    for (int i = i; i < array_size; i++)
+    {
+        array[i] = array[i + 1];
+    }
+    array_size--;
 }
 
 void DynamicArray::output()
 {
     {
         int *p = array;
-        for (int i = 0; i <= array_size; i++)
+        for (int i = 0; i <= array_size - 1; i++)
         {
             std::cout << p[i] << std::endl;
         }
@@ -177,6 +223,17 @@ int main()
 {
     int x[] = {1, 2, 3, 4, 5, 6};
     DynamicArray v(x, 6);
-    v.set_at(30, 999);
+    std::cout << "Delete first:" << std::endl;
+    v.delete_first();
+    v.output();
+    std::cout << "Insert first 10:" << std::endl;
+    v.insert_first(10);
+    v.output();
+    std::cout << "Get index 4:" << v.get_at(4) << std::endl;
+    std::cout << "Insert 100 at index 20:" << std::endl;
+    v.insert_at(20, 100);
+    v.output();
+    std::cout << "set 1 at index 0:" << std::endl;
+    v.set_at(0,1);
     v.output();
 }
