862a1309ab3cdacf841e36583bf627df10b0e187
LeeWendao <LeeWendao@outlook.com>
2022-06-25 11:29:54 +0800

    Update experiment.hs

--- FunctionalProgramming/experiment.hs
+++ FunctionalProgramming/experiment.hs
@@ -71,3 +71,16 @@ g n = n*n
 
 test::Integer -> Integer
 test a = (y.g)a
+
+data Thing = Shoe 
+           | Ship 
+           | SealingWax 
+           | Cabbage 
+           | King
+  deriving Show
+
+data Person = Person String Int Thing
+  deriving Show
+
+baz :: Person -> String
+baz p@(Person n _ _) = "The name field of (" ++ show p ++ ") is " ++ n
