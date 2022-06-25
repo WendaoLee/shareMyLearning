ed5621830165806ae8e7802b099b819456cc3783
LeeWendao <39958730+WendaoLee@users.noreply.github.com>
2022-06-25 12:06:21 +0800

    Update experiment.hs

--- FunctionalProgramming/experiment.hs
+++ FunctionalProgramming/experiment.hs
@@ -2,7 +2,7 @@ lucky :: (Integral a) => a -> String
 lucky x = case x of
   7 -> "test the polymorphic"
   x -> "sorry"
--- t
+-- no harm to the file
 -- lucky :: (Integral a) => a -> String
 -- lucky 7 ="test the polymorphic"
 -- lucky x = "sorry"
